337e2232a776402640f2c9ed08ec92c8e10c73ae
shreeraj007 <shreerajmane007@gmail.com>
2024-02-19 23:03:43 +0530

    Added : Day01

---
+++ day01/learning.js
@@ -0,0 +1,14 @@
+// Write a simple function in javascript
+
+function foo(){
+    console.log('This is the foo function')
+}
+
+function sumOfTwo(a,b){
+    const c = a + b;
+    return c;
+}
+
+const executeThis = sumOfTwo(5,7);
+console.log(executeThis);
+
